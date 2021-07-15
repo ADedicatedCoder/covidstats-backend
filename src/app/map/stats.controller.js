234884a43d71adfa9652565c4a129266f2045598
ADedicatedCoder <mahin.kukreja@gmail.com>
2021-07-15 00:36:08 -0400

    Got global API to function seamlessly

--- src/app/map/stats.controller.js
+++ src/app/map/stats.controller.js
@@ -5,45 +5,26 @@ const https = require("https");
 // Check if the province and hrcode are valid and return statistics for the hr in a JSON format
 const stats = (req, res) => {
     // Variables to hold request URL parameters
-    const province = req.params.province;
-    const hrcode = req.params.hrcode;
+    const country = req.params.country;
 
     // The URL which statistics will be requested from
-    const url = `https://api.opencovid.ca/summary?loc=${hrcode}`;
+    const url = `https://disease.sh/v3/covid-19/countries/Canada?strict=true`;
 
-    // If the province and hrcode are valid, send the HTTPS request and return the information
-    if (
-        provinces[province.toString()] &&
-        provinces[province.toString()].hrs[hrcode.toString()]
-    ) {
-        // Send the HTTPS request
-        https.get(url, (apiRes) => {
-            apiRes.on("data", (data) => {
-                // Parse and format data
-                let statistics = JSON.parse(data);
-                let formattedStats = {
-                    cases: statistics.summary[0].cases,
-                    deaths: statistics.summary[0].deaths,
-                    cumulativeCases: statistics.summary[0].cumulative_cases,
-                    cumulativeDeaths: statistics.summary[0].cumulative_deaths,
-                    hr: statistics.summary[0].health_region,
-                    province: statistics.summary[0].province,
-                    date: statistics.summary[0].date,
-                };
-
-                // Return the stats with a status code of 200 in a JSON format
-                return res.status(200).json({
-                    msg: "Success",
-                    stats: formattedStats,
-                });
+    https.get(url, (apiRes) => {
+        let result = "";
+        apiRes.on("data", (data) => {
+            // Parse and format data
+            result += data;
+        });
+        // Return the stats with a status code of 200 in a JSON format
+        apiRes.on("end", () => {
+            let statistics = JSON.parse(result);
+            return res.status(200).json({
+                msg: "Success",
+                stats: statistics,
             });
         });
-        // If either condition fails, return the error message
-    } else {
-        res.status(404).json({
-            msg: `There is no province and health region with the codes: ${province}, ${hrcode} respectively. Refer back to /map to see all the codes.`,
-        });
-    }
+    });
 };
 
 // Export the function for routes.js
