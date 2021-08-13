6d2203c694efe0469ee368cafd3be36cab10430d
ADedicatedCoder <mahin.kukreja@gmail.com>
2021-08-13 17:31:08 -0400

    Fixed dependency issue and removed /about endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 // Import endpoint response functions from files
 const home = require("../src/app/home/home.controller");
-const about = require("../src/app/about/about.controller");
+// const about = require("../src/app/about/about.controller");
 const map = require("../src/app/map/map.controller");
 const stats = require("../src/app/map/stats.controller");
 const bot = require("../src/app/map/bot.controller");
@@ -11,7 +11,7 @@ const router = express.Router();
 
 // Assign endpoints to corresponding endpoint functions
 router.get("/", home);
-router.get("/about", about);
+// router.get("/about", about);
 router.get("/map", map);
 router.get("/map/:country", stats);
 router.post("/bot", bot);
